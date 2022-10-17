ecbe6abc3506847d468e6ff3b98e574e4911610c
Pape Barka <papebarka@gmail.com>
2022-10-17 20:01:30 +0000

    Starter

---
+++ src/App.svelte
@@ -0,0 +1,5 @@
+<Renderer title="Test" content="No Content" />
+
+<script>
+    import Renderer from "./components/Renderer.svelte"
+</script>
---
+++ src/components/Renderer.svelte
@@ -0,0 +1,20 @@
+{#if title}
+    { title }
+{:else}
+    <h1>Pas de titre</h1>
+{/if}
+
+<div class="">
+    {#if content}
+        {content}
+    {:else}
+        <p>Pas de contenu</p>
+    {/if}
+
+</div>
+
+
+<script>
+    export let title = ''
+    export let content = ''
+</script> 
